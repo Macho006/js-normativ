4c1791893172c6ab710b2591e68a5e238a20dbb4
islomjonov <macbook_uz@MacBook-Air-2.local>
2025-07-17 21:40:56 +0500

    5 ta milos

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-let a = 7
+// 1-savol
+// son = prompt("Sonni kiriting: ")
+
+// if (son % 2 == 0){
+//     console.log("Bu juft son.");
+// } else{
+//     console.log("Bu toq son.");
+// }
+
+//  2-savol
+// let son = +prompt("Sonni kiriting: ")
+
+// if (son > 0){
+//     console.log("musbat");
+// } else if (son == 0) {
+//      console.log("0ga teng");    
+// } else{
+//     console.log("manfiy");
+// }
+
+//  3-savol
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+
+// function sum() {
+//     if (a > b) {
+//         console.log("a son b sondan kotta");
+//     } else if (a == b) {
+//         console.log("a son b songa teng");
+//     } else {
+//         console.log("b son a sondan kotta");
+//     }
+// }
+// sum()
+
+// 4-savol
+// let ball = prompt("Talabaning olgan balini kiriting");
+
+// if (ball >= 1 && ball <= 59) {
+//     console.log("Talabaning bahosi 2, imtihondan o'tmadi");
+// } else if (ball >= 60 && ball <= 69) {
+//     console.log("Talabaning bahosi 3, imtihondan o'tdi");
+// } else if (ball >= 70 && ball <= 89) {
+//     console.log("Talabaning bahosi 4, imtihondan o'tdi");
+// } else if (ball >= 90 && ball <= 100) {
+//     console.log("Talabaning bahosi 5, imtihondan o'tdi");
+// } else {
+//     console.log("Noto'g'ri ball kiritildi!");
+// }
+
+// 5-savol
+// function tekshiruv() {
+//     const togriLogin = "ustudy2025";
+//     const togriParol = "U9frontend";
+
+//     const login = prompt("Loginni kiriting:");
+//     const parol = prompt("Parolni kiriting:");
+
+//     if (login === togriLogin && parol === togriParol) {
+//         console.log("Muvaffaqiyatli");
+//     } else if (login !== togriLogin && parol === togriParol) {
+//         console.log("Kiritilgan login xato");
+//     } else if (login === togriLogin && parol !== togriParol) {
+//         console.log("Kiritilgan password xato");
+//     } else {
+//         console.log("Kiritilgan login va password xato");
+//     }
+// }
+
+// tekshiruv();
+
